efa274216340b6ad90040db6538859fe3f979ddd
marcelobrgomes <marcelobrgomes@gmail.com>
2016-08-27 17:29:49 -0300

    adicionando o middleware method-override para tratar as requisições put e delete

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -11,5 +11,15 @@ module.exports = function() {
         res.json(contatos);
     };
     
+    controller.getContato = function(req, res) {
+        var idContato = req.params.id;
+        
+        var contato = contatos.filter(function(contato) {
+            return contato._id == idContato;
+        })[0];
+        
+        contato ? res.json(contato) : res.status(404).send("Contato não encontrado");
+    };
+    
     return controller;
 };
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 //A variável express armazena uma função que retorna uma instância do Express.
 var express = require("express");
 var load = require("express-load");
+var bodyParser = require("body-parser");
 
 //module.exports é retornado quando executado o require
 module.exports = function() {
@@ -11,6 +12,9 @@ module.exports = function() {
     
     //middleware
     app.use(express.static("./public"));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require("method-override")());
     
     //Template engine EJS
     app.set("view engine", "ejs");
